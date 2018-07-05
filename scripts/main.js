1d1cc76d2bc16a4feacef458c59ca328c21b9be7
George Benjamin <msdcconnect@gmail.com>
2018-07-05 13:55:54 +0100

    Styled list item

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
-console.log("Javascript has been added!")
+var add_button = document.getElementById("add-todo");
+add_button.addEventListener('click', addTodoItem);
+
+function addTodoItem(){
+    var todoItem = document.getElementById('todo-input').value;
+    console.log(todoItem);
+}
