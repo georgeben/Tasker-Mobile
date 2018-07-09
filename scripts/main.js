a6b654bbec0c3c4107a72ddefab517ad4cae5bd3
George Benjamin <msdcconnect@gmail.com>
2018-07-09 12:59:18 +0100

    Refactored code'

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 
-var data = (localStorage.getItem('tasks'))? JSON.parse(localStorage.getItem('tasks')) : {
-    todo:[],
-    completed:[]
+var taskData = (localStorage.getItem('tasks'))? JSON.parse(localStorage.getItem('tasks')) : {
+    uncompletedTasks:[],
+    completedTasks:[]
 }
 
 var showCompleteLabel = false;
@@ -10,16 +10,16 @@ displayData();
 
 function displayData(){
     //Displays the stored tasks on th elist items
-    if(!data.todo.length && !data.completed.length) return;
+    if(!taskData.uncompletedTasks.length && !taskData.completedTasks.length) return;
     
-    for(let i = 0; i < data.todo.length; i++){
-        var task = data.todo[i];
-        addItem(task);
+    for(let i = 0; i < taskData.uncompletedTasks.length; i++){
+        var task = taskData.uncompletedTasks[i];
+        addItemToDOM(task);
     }
     
-    for(let j = 0; j < data.completed.length; j++){
-        var task = data.completed[j];
-        addItem(task, completed);
+    for(let j = 0; j < taskData.completedTasks.length; j++){
+        var task = taskData.completedTasks[j];
+        addItemToDOM(task, true);
 
     }
 }
@@ -27,7 +27,7 @@ function displayData(){
 function storeDataLocally(){
     //This function gets called everytime the data object is being updated
     //JSON.stringify() converts a javascript object to JSON
-    localStorage.setItem('tasks', JSON.stringify(data));
+    localStorage.setItem('tasks', JSON.stringify(taskData));
 }
 
 
@@ -35,7 +35,7 @@ var add_button = document.getElementById('add-todo');
 
 var inputField = document.getElementById('input');
 
-add_button.addEventListener('click', addItemTodo);
+add_button.addEventListener('click', validateInput);
 
 inputField.addEventListener("keypress", function(event){
     if(event.which === 13){
@@ -46,52 +46,52 @@ inputField.addEventListener("keypress", function(event){
 
 
 
-function addItemTodo(){
+/*function addItemTodo(){
      validateInput()
-}
+}*/
 
 function validateInput(){
     var userInput = inputField.value;
     userInput = userInput.trim();
     if(userInput){
-        data.todo.push(userInput);
+        taskData.uncompletedTasks.push(userInput);
         storeDataLocally();
-        addItem(userInput);
+        addItemToDOM(userInput);
         inputField.value = '';
     }
 }
 
 function removeTodoItem(){
-    var item = this.parentNode.parentNode;
-    var parent = item.parentNode;
+    var listItemClicked = this.parentNode.parentNode;
+    var parent = listItemClicked.parentNode;
     var parentId = parent.id;
-    var todoText = item.innerText;
+    var task = listItemClicked.innerText;
     
     if(parentId === "todo"){
-        data.todo.splice(data.todo.indexOf(todoText), 1);
+        taskData.uncompletedTasks.splice(taskData.uncompletedTasks.indexOf(task), 1);
     }else{
-        data.completed.splice(data.completed.indexOf(todoText), 1);   
+        taskData.completedTasks.splice(taskData.completedTasks.indexOf(task), 1);   
     }
     
     storeDataLocally();
     
-    parent.removeChild(item);
+    parent.removeChild(listItemClicked);
 }
 
 function completeTodo(){
-    var item = this.parentNode.parentNode; //Retrieves the list item which button was clicked
-    var parent = item.parentNode; //Retrieves the list(ul) associated with that list item
+    var listItemClicked = this.parentNode.parentNode; //Retrieves the list item which button was clicked
+    var parent = listItemClicked.parentNode; //Retrieves the list(ul) associated with that list item
     var parentId = parent.id; //Retrieves the id of the list
     
     //
-    var todoText = item.innerText;
+    var task = listItemClicked.innerText;
     
     if(parentId === "todo"){
-        data.todo.splice(data.todo.indexOf(todoText), 1);
-        data.completed.push(todoText);
+        taskData.uncompletedTasks.splice(taskData.uncompletedTasks.indexOf(task), 1);
+        taskData.completedTasks.push(task);
     }else{
-        data.completed.splice(data.completed.indexOf(todoText), 1);
-        data.todo.push(todoText);
+        taskData.completedTasks.splice(taskData.completedTasks.indexOf(task), 1);
+        taskData.uncompletedTasks.push(task);
     }
     
     storeDataLocally();
@@ -101,21 +101,21 @@ function completeTodo(){
     
     //Display a label on the completed list
     var completedItems = document.getElementById('completed').childElementCount;
-    if(completedItems === 0){
+    if(completedItems === 0 && !showCompleteLabel){
         var completedLabel = document.createElement('h4');
         completedLabel.appendChild(document.createTextNode("Completed tasks"));
         document.getElementById('todo').appendChild(completedLabel);
     }
     
-    parent.removeChild(item);
-    target.insertBefore(item, target.childNodes[0]);
+    parent.removeChild(listItemClicked);
+    target.insertBefore(listItemClicked, target.childNodes[0]);
 }
 
-function addItem(todoItem, completed){
+function addItemToDOM(task, completed){
     //console.log(todoItem);
     var list_item = document.createElement('li');
     list_item.classList.add('todo-list');
-    list_item.innerHTML = todoItem;
+    list_item.innerHTML = task;
     
     var buttons = document.createElement('div');
     buttons.classList.add('buttons');
@@ -128,7 +128,7 @@ function addItem(todoItem, completed){
     done_btn.innerHTML = "<img src='images/done_icon.png'>";
     done_btn.addEventListener('click', completeTodo);
     
-    var todoList = (completed)? document.getElementById("completed") : document.getElementById('todo');
+    var taskList = (completed)? document.getElementById("completed") : document.getElementById('todo');
     
     if(completed  && !showCompleteLabel){
         var completedLabel = document.createElement('h4');
@@ -144,5 +144,5 @@ function addItem(todoItem, completed){
     list_item.appendChild(buttons);
     
     //Adds the new list item to the top of the list
-    todoList.insertBefore(list_item, todoList.childNodes[0]);
+    taskList.insertBefore(list_item, taskList.childNodes[0]);
 }
